7dcd43d58af6ad220ca447e6b432bf649616f902
alex <meandu229@protonmail.com>
2023-12-04 10:28:27 +0000

    D4 P2

--- Days/4/Src/Parser.cs
+++ Days/4/Src/Parser.cs
@@ -11,26 +11,31 @@ public class Day4Service
             var numberSplit = idSplit[1].Split("|");
             return new Card()
             {
+                Count = 1,
                 Id = int.Parse(idSplit[0].Split(" ").Where(x=> !string.IsNullOrEmpty(x)).ToArray()[1]),
                 CardNumbers = numberSplit[1].Split(" ").Where(x=> !string.IsNullOrEmpty(x)).Select(y => int.Parse(y)).ToList(),
                 WinningNumbers = numberSplit[0].Split(" ").Where(x=> !string.IsNullOrEmpty(x)).Select(y => int.Parse(y)).ToList(),
             };
+        }).ToList();
+
+        cards.ForEach(x =>
+        {
+            var intetsect = x.CardNumbers.Intersect(x.WinningNumbers).Count();
+            for (int i = 0; i < intetsect; i++)
+            {
+                cards[x.Id + i].Count += x.Count;
+            }
         });
-        
-        var scores = cards.ToList().Select(x => x.CardNumbers.Intersect(x.WinningNumbers)).Select(x => Score(x));
-        return scores.Sum();
+        return cards.Select(x => x.Count).Sum();
     }
 
     private static int Score(IEnumerable<int> enumerable)
     {
-        if (enumerable.Count() == 0)
-            return 0;
-        var counter = 1;
-        foreach (var score in enumerable.Skip(1))
+        var counter = 0;
+        foreach (var score in enumerable)
         {
-            counter = counter * 2;
+            counter =+ 1;
         }
-
         return counter;
     }
 }
@@ -40,4 +45,5 @@ public class Card
     public int Id { get; set; }
     public List<int> WinningNumbers { get; set; }
     public List<int> CardNumbers { get; set; }
+    public int Count { get; set; }
 }
